a25446818de747c24b3226f874c906dd80965461
Nahuel Sosa <109542769+nahuelsosa122@users.noreply.github.com>
2023-03-01 15:27:37 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import { suma,multiplicacion } from "./controller";
+import chalk from "chalk";
+
+// console.log(multiplicacion(suma(2,3), suma(4,5)))
+console.log(chalk.green(multiplica(suma(2, 3), suma(4, 5))))
